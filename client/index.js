b4de78f93d50e82a8112eddfdf6479af19591e21
Ionut Ciucle <ionut.ciucle@3pillarglobal.com>
2017-10-26 17:22:40 +0300

    Added justification for the timeout in index.js

--- client/index.js
+++ client/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './app-container';
 
-// We set a timeout
+// We set a timeout for being able to see
+// the differences between the raw HTML and
+// the React-injected app
+
 setTimeout(function() {
   ReactDOM.hydrate(
     <AppContainer/>,
